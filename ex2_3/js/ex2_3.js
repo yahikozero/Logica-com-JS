a7a692d720c2a2dd220bc43dbbb83d03e2889d13
Thiago Assis Onofre <thiagoao90@gmail.com>
2026-05-25 14:52:24 +0000

    Exemplo 2.3

---
+++ ex2_3/js/ex2_3.js
@@ -0,0 +1,18 @@
+const forms = document.querySelector("form");
+const resp1 = document.querySelector("#outResp1");
+const resp2 = document.querySelector("#outResp2");
+const resp3 = document.querySelector("#outResp3");
+
+forms.addEventListener("submit", (e) =>{
+    const modelo = forms.inVeiculo.value;
+    const preco = forms.inPreco.value;
+
+    const entrada = preco * 0.50;
+    const parcela = (preco * 0.50) / 12;
+
+    resp1.innerText = `Promoção ${modelo}`;
+    resp2.innerText = `Entrada de R$ ${entrada}`;
+    resp3.innerText = `+12x de R$ ${parcela.toFixed(2)}`;
+
+    e.preventDefault();
+});

85f5a8172a344edd02af73e5547997903c1815c3
Thiago Assis Onofre <thiagoao90@gmail.com>
2026-05-26 01:21:58 +0000

    Exercício 2-c

--- Exercícios/resp2_c/resp2_c.js
+++ Exercícios/resp2_c/resp2_c.js
@@ -6,11 +6,12 @@ forms.addEventListener("submit", (e) =>{
     const produto = forms.produto.value;
     const precoProduto = Number(forms.precoProduto.value);
 
-    const promoPreco = (precoProduto * 2) + (precoProduto * 0.50);
-    const precoUtlProduto = (precoProduto * 0.50);
+    const precoUltProduto = (precoProduto * 0.50);
+    const promoPreco = (precoProduto * 2) + precoUltProduto;
+    
 
     promo.innerText = `${produto} - Promoção: Leve 4 e pague R$ ${promoPreco.toFixed(2)}`;
-    valorPromo.innerText = `O 3º produto sai por apenas R$ ${precoUtlProduto.toFixed(2)}`;
+    valorPromo.innerText = `O 3º produto sai por apenas R$ ${precoUltProduto.toFixed(2)}`;
 
     e.preventDefault();
 });

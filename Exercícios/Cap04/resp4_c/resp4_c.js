1e0942f6ddc03041047f4163b0deefc4a536f5c1
Thiago Assis Onofre <thiagoao90@gmail.com>
2026-06-02 12:06:12 +0000

    Exercício 4-c

--- Exercícios/Cap04/resp4_c/resp4_c.js
+++ Exercícios/Cap04/resp4_c/resp4_c.js
@@ -5,17 +5,14 @@ const resp2 = document.querySelector("#resp2");
 forms.addEventListener("submit", (e) => {
     e.preventDefault()
     const deposito = Number(forms.deposito.value)
-    
     if (deposito < 1) {
         alert("Valor insuficiente, valor mínimo: R$1.00");
     };
-
     let troco
     let tempo
-
     if (deposito >= 3) {
         tempo = 120
-        troco = deposito - 3;     
+        troco = deposito - 3;
     } else if (deposito >= 1.75) {
         tempo = 60
         troco = deposito - 1.75;
@@ -23,9 +20,8 @@ forms.addEventListener("submit", (e) => {
         tempo = 30
         troco = deposito - 1;
     }
-
     resp1.innerText = `Tempo: ${tempo} min`;
-    if (troco > 0){
+    if (troco > 0) {
         resp2.innerText = `Troco: R$ ${troco}`;
     }
 });
